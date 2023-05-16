0d9f6166bd67b4825761a25de06d4586b3c8c101
Расул Галеев <RasulGaleev2005@yandex.ru>
2023-05-17 01:58:04 +0300

    Добавил форму регистрации

---
+++ lr12/RegForm.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lr12
+{
+    public partial class RegForm : Form
+    {
+        private readonly AppDbContext _context;
+
+        public RegForm(AppDbContext context)
+        {
+            InitializeComponent();
+            _context = context;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var user = new User
+            {
+                Name = textBox1.Text,
+                Surname = textBox3.Text,
+                Role = textBox2.Text,
+                Phone = textBox1.Text,
+                Email = textBox3.Text,
+                Password = textBox2.Text
+            };
+
+            _context.User.Add(user);
+            _context.SaveChanges();
+
+            MessageBox.Show("Вы успешно зарегистрировались!", "Регистрация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Close();
+
+        }
+    }
+}
